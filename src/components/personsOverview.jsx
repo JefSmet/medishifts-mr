f30ce2fabc557af63359c47f19c5dc258a9e2bf8
JefSmetTl <jef.smet@student.kdg.be>
2024-08-07 10:45:45 +0200

    removed dummy data now getting data from database

--- src/components/personsOverview.jsx
+++ src/components/personsOverview.jsx
@@ -3,28 +3,39 @@ import DeletePerson from './deletePerson';
 import PersonSelector from './personSelector';
 import UpdatePerson from './updatePerson';
 import axios from 'axios';
+import { useState } from 'react';
 
 const apiRoute = import.meta.env.VITE_API_ROUTE + '/persons';
 
 export default function PersonsOverview() {
-  let persons;
+  const [persons, setPersons] = useState([]);
+  const [isLoading, setIsLoading] = useState(true);
+
   useEffect(() => {
     axios
       .get(apiRoute)
       .then((response) => {
-        persons = response.data;
+        setPersons(response.data);
+        console.log('resonse', response.data);
+        setIsLoading(false);
       })
       .catch((error) => {
         console.error(error);
+        setIsLoading(false);
       });
   }, []);
+
+  if (isLoading) {
+    return <div>Loading...</div>;
+  }
+  console.log(persons);
   return (
     <div>
       <div className="flex items-center justify-between">
         <h1 className="mb-4 text-3xl font-bold">Persons</h1>
       </div>
       <div>
-        <PersonSelector />
+        <PersonSelector persons={persons} />
       </div>
       <UpdatePerson />
       <DeletePerson />
