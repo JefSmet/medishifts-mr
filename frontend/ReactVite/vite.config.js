c130abc1d0a6e135993b46c1af5171bd16fee128
JefSmetTl <jef.smet@student.kdg.be>
2024-08-16 17:16:42 +0200

    transferred over some necessary files

--- frontend/ReactVite/vite.config.js
+++ frontend/ReactVite/vite.config.js
@@ -1,13 +1,8 @@
-import react from '@vitejs/plugin-react';
+import react from '@vitejs/plugin-react-swc';
 import { defineConfig } from 'vite';
+import { svgrComponent } from 'vite-plugin-svgr-component';
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  plugins: [react()],
-  build: {
-    sourcemap: true,
-  },
-  server: {
-    sourcemap: true,
-  },
+  plugins: [react(), svgrComponent()],
 });
