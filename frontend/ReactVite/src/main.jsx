580ab284b4722d7da6de1004d30734b460379eeb
JefSmetTl <jef.smet@student.kdg.be>
2024-08-21 21:34:47 +0200

    added styling for verlof requests

--- frontend/ReactVite/src/main.jsx
+++ frontend/ReactVite/src/main.jsx
@@ -21,6 +21,7 @@ import UpdateUsers from './components/updatePersons.jsx';
 import UpdateActivityType from './components/updateActivityType.jsx';
 import Login from './components/login.jsx';
 import VerlofAanvragen from './routes/verlofAanvragen.jsx';
+import VerlofRequests from './routes/verlofRequests.jsx';
 const router = createBrowserRouter(
   createRoutesFromElements(
     <Route path="/" element={<Root />} errorElement={<ErrorPage />}>
@@ -35,6 +36,7 @@ const router = createBrowserRouter(
         action={createPersonAction}
       />
       <Route path="verlofAanvragen" element={<VerlofAanvragen />} />
+      <Route path="verlofRequests" element={<VerlofRequests />} />
     </Route>,
   ),
 );
